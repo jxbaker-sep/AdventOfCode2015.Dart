d85ef48836b0b47d19be8f3615d10eabe4765791
Jake Baker <SEP.Baker@fronius.com>
2024-07-11 10:26:06 -0400

    day25 redux

--- day25.dart
+++ day25.dart
@@ -1,4 +1,7 @@
 
+import 'utils/input.dart';
+import 'utils/test.dart';
+
 int p(int n) => n * (n+1) ~/ 2;
 
 int v(int r, int c) => p(c) + p(c+r-2) - p(c-1);
@@ -6,12 +9,13 @@ int v(int r, int c) => p(c) + p(c+r-2) - p(c-1);
 int d25(int r, int c) {
   final n = v(r,c)-1;
   var x = 20151125;
-  for (final i in Iterable.generate(n)) {
+  for (final _ in Iterable.generate(n)) {
     x = (x*252533)%33554393;
   }
   return x;
 }
 
-void main() {
-  print(d25(3010,3019));
+Future<void> main() async {
+  final input = (await getInput('day25')).split(',').map(int.parse).toList();
+  test(d25(input[0], input[1]), 8997277);
 }
